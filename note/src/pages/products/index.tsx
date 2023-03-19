ae06e49193e2c49c7a4dafddc91301bdba389054
DongGyu <ge7979@gmail.com>
2023-03-19 20:35:56 +0900

    정적 라우팅 공부

---
+++ note/src/pages/products/index.tsx
@@ -0,0 +1,3 @@
+export default function ProductsPage(){
+    return <h1>제품소개 페이지!</h1>;
+}
---
+++ note/src/pages/products/pants.tsx
@@ -0,0 +1,4 @@
+// 정적 라우팅 12버전
+export default function PantsPage(){
+    return <h1>바지 제품 설명 페이지!</h1>;
+}
