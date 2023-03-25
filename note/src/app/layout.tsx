dc73affd230656177e2440e703bd0de7ee62c7c2
DongGyu <ge7979@gmail.com>
2023-03-25 11:24:07 +0900

    layout 골격 만들기
    layout 골격 만들기 연습

--- note/src/app/layout.tsx
+++ note/src/app/layout.tsx
@@ -1,18 +1,27 @@
 import './globals.css'
-
+import styles from './layout.module.css';
 export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
+                                       children,
+                                   }: {
+    children: React.ReactNode
 }) {
-  return (
-    <html lang="en">
-      {/*
+    return (
+        <html lang="en">
+        {/*
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
-      <head />
-      <body>{children}</body>
-    </html>
-  )
+        <head/>
+        <body>
+        <header className={styles.header}>
+            <h1>Demo Note App</h1>
+            <nav className={styles.nav}>
+                <a href="">Contact</a>
+                <a href="">About</a>
+            </nav>
+        </header>
+        {children}
+        </body>
+        </html>
+    );
 }
